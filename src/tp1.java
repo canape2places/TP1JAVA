85e0992ddce0b98d6d9fe3d9e5bc24d3ab716972
Enzo Borras <borras.enzo@etu.educ-valadon-limoges.fr>
2023-01-17 15:36:11 +0100

    Initial commit

---
+++ src/tp1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class tp1
+{
+        public static void main(String[] args)
+        {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Saisissez votre prénom");
+            String prenom = sc.next();
+            System.out.println(prenom);
+    }
+
+}
